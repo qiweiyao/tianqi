9ec3b2e39de7a2f14fb4df45ef7b3665d604728d
qwy <qwy@www.sn.10086.cn>
2016-12-05 15:06:10 +0800

    Initial Commit

---
+++ 123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by qwy on 16/12/5.
+//  Copyright © 2016年 qwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 123/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  123
+//
+//  Created by qwy on 16/12/5.
+//  Copyright © 2016年 qwy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
